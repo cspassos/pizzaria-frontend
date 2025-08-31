3d6bb1a19c601f22abefde3503d94999b0ad3bdd
cspassos <caic.souza@hotmail.com>
2025-08-31 12:23:13 -0300

    Projeto web pizzaria

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
-// services/api.ts
+// src/services/api.ts
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API,
-  // timeout, etc. se quiser
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 });
+
+export default api;
